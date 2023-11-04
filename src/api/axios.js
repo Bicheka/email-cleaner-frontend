ee41acc0ab7daf65a0f8173cbd8825962543c2dc
David Martinez Gil <davidmartinezgilp@gmail.com>
2023-11-04 15:11:48 -0400

    form updated and error handling added to form
    now a specific error message will appear depending of the response status number
    * styles added
    * material ui loading component added and working good
    * try again button working good
    * success and error icon svg added for the message response

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-    baseURL: "http://localhost:3000",
-    timeout: 3000,
+    baseURL: "http://localhost:8000",
+    timeout: 10000,
     headers: {
         "Content-Type": "application/json",
     },
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,24 +1,80 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import '../css/Form.css';
+
+// Loading icon
+import Loading from "./Loading";
+
 const Form = () => {
     const [email, setEmail] = useState("");
-    const [phone, setPhone] = useState("");
-    const [isSubmittingForm, setIsSubmittingForm] = useState(true);
-    const [repeat, setRepeat] = useState(false);
+    const [password, setPassword] = useState("");
+    const [isSubmittingForm, setIsSubmittingForm] = useState(true); // Change this to true
+    const [formSubmissionStatus, setFormSubmissionStatus] = useState(null); // Added form submission status
+    const [message, setMessage] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [response, setResponse] = useState(null);
+    const [error, setError] = useState(""); // Added error state
 
-    const handleCheckBoxChange = () =>{
-        setRepeat(!repeat);
-    }
-
-    const handleSubmit = (e) =>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        setIsSubmittingForm(false);
+
+        setIsSubmittingForm(false); // Set isSubmittingForm to true when the form is submitted
+
+        try {
+            const response = await axios.post("http://localhost:8000/clear", {
+                email: email,
+                password: password,
+            });
+            console.log(response);
+            setResponse(response);
+            setMessage(response.data.message);
+            setFormSubmissionStatus("success"); // Set form submission status to success
+        } catch (error) {
+            
+            setResponse(error);
+
+            // Set error message based on the status code
+            if (error.response.status === 401) {
+                setError("Invalid credentials.");
+            }
+            else if (error.response.status === 422) {
+                setError("Invalid email address.");
+            }
+            else if (error.response.status === 500) {
+                setError("An internal server error occurred.");
+            }
+            else {
+                setError("An error occurred.");
+            }
+            setFormSubmissionStatus("error"); // Set form submission status to error
+
+        } finally {
+            setIsSubmittingForm(false); // Set isSubmittingForm to false after the response is received
+        }
     }
-    
+
+    const handleReset = () => {
+        setEmail("");
+        setPassword("");
+        setIsSubmittingForm(true);
+        setFormSubmissionStatus(null);
+        setMessage("");
+        setError("");
+        setResponse(null);
+        setLoading(true);
+        
+    }
+
+    useEffect(() => {
+        if (response) {
+            setLoading(false);
+        }
+    }, [response]);
+
     return (
-       <div className="form-container">
-            {isSubmittingForm ? 
-                <form className="" onSubmit={handleSubmit} >
+        <div className="form-container">
+            {isSubmittingForm ? (
+                <form onSubmit={handleSubmit}>
                     <div className="category">
                         <label>Email</label>
                         <input
@@ -31,37 +87,41 @@ const Form = () => {
                         <label>App Password</label>
                         <input
                             type="password"
-                            value={phone}
-                            onChange={(e) => setPhone(e.target.value)}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required={true}
                         />
-                    </div> 
-                    <div className="category">
-                        <input 
-                            className="repeat-checkbox"
-                            type="checkbox"
-                            value={repeat}
-                            onChange={handleCheckBoxChange}
-                        />
-                        <label>Repeat</label>
-                        {
-                            repeat && 
-                            <select>
-                                <option value="">Select frequence</option>
-                                <option value="Daily">Dayly</option>
-                                <option value="Weekly">Weekly</option>
-                                <option value="Monthly">Monthly</option>
-                            </select>
-                        }
                     </div>
-
-                    <button type="submit" >Submit</button>
+                    <button type="submit">Submit</button>
                 </form>
-            :
-                <div className="success-message">
-                    <p>Form submitted successfully!</p>
+            ) : (
+                loading ? (
+                    <Loading />
+                ) : (
+                    <div className="success-message">
+                    {formSubmissionStatus === "success" ? (
+                        // Render the Loading component within this block
+                        <div>
+                            <h2>Success!</h2>
+                            <img className="success-icon" src="../public/success.svg" alt="Success" />
+                            <p>{message}.</p>
+                            
+                            <button className="try-again-button" onClick={() => handleReset()}>Again?</button>
+                            
+                        </div>
+            
+                    ) : formSubmissionStatus === "error" ? (
+                        <div className="error-message">
+                            <h2>Error!</h2>
+                            <img className="error-icon" src="../public/error.svg" alt="Error" />
+                            <p>{error} Please try again</p>
+                            <button className="try-again-button" onClick={() => handleReset()}>Try Again</button>
+                        </div>
+                    ) : null}
                 </div>
-        }
-       </div>
+                )
+            )}
+        </div>
     );
 }
 
