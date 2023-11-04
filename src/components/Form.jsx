cf45f195b44578f9c21aa13e4d2c5badac53821b
David Martinez Gil <davidmartinezgilp@gmail.com>
2023-11-03 22:28:14 -0400

    success message added when form is submitted

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,51 +3,65 @@ import '../css/Form.css';
 const Form = () => {
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
+    const [isSubmittingForm, setIsSubmittingForm] = useState(true);
     const [repeat, setRepeat] = useState(false);
 
     const handleCheckBoxChange = () =>{
         setRepeat(!repeat);
     }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        setIsSubmittingForm(false);
+    }
     
     return (
-        <form className="">
-            <div className="category">
-                <label>Email</label>
-                <input
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </div>
-           <div className="category">
-                <label>App Password</label>
-                <input
-                    type="password"
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                />
-           </div> 
-            <div className="category">
-                <input 
-                    className="repeat-checkbox"
-                    type="checkbox"
-                    value={repeat}
-                    onChange={handleCheckBoxChange}
-                />
-                <label>Repeat</label>
-                {
-                    repeat && 
-                    <select>
-                        <option value="">Select frequence</option>
-                        <option value="Daily">Dayly</option>
-                        <option value="Weekly">Weekly</option>
-                        <option value="Monthly">Monthly</option>
-                    </select>
-                }
-           </div>
+       <div className="form-container">
+            {isSubmittingForm ? 
+                <form className="" onSubmit={handleSubmit} >
+                    <div className="category">
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="category">
+                        <label>App Password</label>
+                        <input
+                            type="password"
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                        />
+                    </div> 
+                    <div className="category">
+                        <input 
+                            className="repeat-checkbox"
+                            type="checkbox"
+                            value={repeat}
+                            onChange={handleCheckBoxChange}
+                        />
+                        <label>Repeat</label>
+                        {
+                            repeat && 
+                            <select>
+                                <option value="">Select frequence</option>
+                                <option value="Daily">Dayly</option>
+                                <option value="Weekly">Weekly</option>
+                                <option value="Monthly">Monthly</option>
+                            </select>
+                        }
+                    </div>
 
-            <button type="submit">Submit</button>
-        </form>
+                    <button type="submit" >Submit</button>
+                </form>
+            :
+                <div className="success-message">
+                    <p>Form submitted successfully!</p>
+                </div>
+        }
+       </div>
     );
 }
 
